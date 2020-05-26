dc7f79aad41b01112e17638b9e7d111500910741
Matt Goff <mgoff72@gmail.com>
2020-05-26 09:49:56 -0700

    first commit

---
+++ find_the_odd_int/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace find_the_odd_int
+{
+
+    class Kata
+    {
+    public static int find_it(int[] seq)
+      {
+          for (int i = 0; i < seq.Length; i++)
+          {
+              var currentInt = seq[i];
+              var currentIntCount = 0;
+
+            foreach (var item in seq)
+            {
+                if (item == currentInt) currentIntCount ++;
+            }
+
+            if ( currentIntCount % 2 != 0) return currentInt;
+          }
+      return -1;
+      }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var testResults = Kata.find_it ( new[] { 20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5 });
+            System.Console.WriteLine($"Results: {testResults}");
+        }
+    }
+}
---
+++ friend_or_foe/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace friend_or_foe
+{
+  public static class Kata {
+  public static IEnumerable<string> FriendOrFoe (string[] names) {
+
+    List<string> returnList = new List<string>();
+
+    foreach (var name in names)
+    {
+        if(name.Length == 4) returnList.Add(name);
+    }
+    return returnList;
+  }
+  }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] expected = { "Ryan", "Mark" };
+            string[] names = { "Ryan", "Kieran", "Mark", "Jimmy" };
+            var results = Kata.FriendOrFoe(names);
+            System.Console.WriteLine(results.ToString());
+        }
+    }
+}
