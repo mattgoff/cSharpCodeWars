68f79960a07c729e7f5ba86d53853ff5d131d22a
Matt Goff <matt@goff.us>
2020-09-22 15:27:38 -0700

    updates

---
+++ holiday_vi_-_shark_pontoon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace holiday_vi___shark_pontoon
+{
+
+  public class Kata
+  {
+    public static string Shark(
+      int pontoonDistance,
+      int sharkDistance,
+      int yourSpeed,
+      int sharkSpeed,
+      bool dolphin)
+    {
+
+        var myTime = pontoonDistance / yourSpeed;
+        var sharkTime = dolphin ? sharkDistance / (sharkSpeed / 2) : sharkDistance / sharkSpeed;
+
+        return (myTime < sharkTime) ? "Alive!" : "Shark Bait!";
+    }
+  }
+    class Program
+    {
+      static void Main(string[] args)
+      {
+        Console.WriteLine("Hello World!");
+        System.Console.WriteLine($"Alive - {Kata.Shark(12, 50, 4, 8, true)}");
+        System.Console.WriteLine($"Alive - {Kata.Shark(12, 50, 4, 8, false)}");
+        System.Console.WriteLine($"Alive - {Kata.Shark(7, 55, 4, 16, true)}");
+        System.Console.WriteLine($"Shark Bait - {Kata.Shark(24, 0, 4, 8, true)}");
+        System.Console.WriteLine($"Shark Bait - {Kata.Shark(40, 35, 3, 20, true)}");
+        System.Console.WriteLine($"Alive - {Kata.Shark(7, 8, 3, 4, true)}");
+        System.Console.WriteLine($"Shark Bait - {Kata.Shark(7, 8, 3, 4, false)}");
+      }
+    }
+  }
---
+++ the_falling_speed_of_petals/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace the_falling_speed_of_petals
+{
+  public static class Kata
+  {
+    public static double SakuraFall(double v)
+    {
+      if (v <= 0) return 0;
+      double fallDistance = 80 * 5;
+      return fallDistance / v;
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var result = Kata.SakuraFall(5);
+      System.Console.WriteLine($"Result is {result}");
+    }
+  }
+}
