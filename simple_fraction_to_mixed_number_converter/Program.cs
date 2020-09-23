c15c7cddf90c011f1b7225b54aea8c2fe5892fcc
Matt Goff <matt@goff.us>
2020-09-23 09:23:15 -0700

    more

---
+++ simple_fraction_to_mixed_number_converter/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace simple_fraction_to_mixed_number_converter
+{
+  public class Kata
+  {
+    public static string MixedFraction(string s)
+    {
+      var stringSplit = s.Split("/");
+      var numerator = Int32.Parse(stringSplit[0]);
+      var denominator = Int32.Parse(stringSplit[1]);
+
+
+      if (numerator % denominator == 0)
+      {
+        return (numerator / denominator).ToString();
+      }
+      else
+      {
+        var wholeNumber = numerator / denominator;
+        var nonSimplifiedNumerator = Math.Abs(numerator) - Math.Abs(denominator * wholeNumber);
+
+        var gcdValue = GCD(nonSimplifiedNumerator, denominator);
+        var simplifiedNumerator = nonSimplifiedNumerator / gcdValue;
+        var simplifiedDenominator = denominator / gcdValue;
+
+        return wholeNumber == 0 ? $"{simplifiedNumerator}/{simplifiedDenominator}" : $"{wholeNumber} {simplifiedNumerator}/{simplifiedDenominator}";
+      }
+    }
+
+    private static int GCD(int a, int b)
+    {
+      while (a != 0 && b != 0)
+      {
+        if (a > b)
+          a %= b;
+        else
+          b %= a;
+      }
+
+      return a | b;
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Kata.MixedFraction("42/9"));
+    }
+  }
+}
---
+++ substituting_variables_into_strings_padded_numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace substituting_variables_into_strings_padded_numbers
+{
+  public class PaddedNumbers
+  {
+    public static string Solution(int value)
+    {
+      var valueLength = value.ToString().Length;
+      string padding = new string('0', 5 - valueLength);
+      return $"Value is {padding}{value}";
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(PaddedNumbers.Solution(5));
+    }
+  }
+}
