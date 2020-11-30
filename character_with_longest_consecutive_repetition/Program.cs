fdc8484b73ebf8b28ce14014ead4afeb3c055bb2
Matt Goff <matt@goff.us>
2020-11-30 16:49:11 -0700

    a new one

---
+++ character_with_longest_consecutive_repetition/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace character_with_longest_consecutive_repetition
+{
+
+  public static class Kata
+  {
+    public static Tuple<char?, int> LongestRepetition(string input)
+    {
+        if (input == String.Empty)
+        {
+            return new Tuple<char?, int>(null, 0);
+        }
+
+        int currentCounter = 1;
+        int longestCounter = 0;
+        char longestChar = input[0];
+        char previousChar = char.MinValue;
+
+        foreach (var character in input)
+        {
+            if (character != previousChar)
+            {
+                currentCounter = 1 ;
+            }
+            else
+            {
+                currentCounter ++;
+            }
+
+            if (currentCounter > longestCounter)
+            {
+                longestChar = character;
+                longestCounter = currentCounter;
+            }
+            previousChar = character;
+        }
+        return new Tuple<char?, int>( longestChar, longestCounter);
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      var myResult = Kata.LongestRepetition("aaabbbcccc");
+      System.Console.WriteLine($"Result => {myResult}");
+    }
+  }
+}
---
+++ reversed_sequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace reversed_sequence
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Kata.ReverseSeq(5);
+
+    //    Assert.That(Kata.ReverseSeq(5), Is.EqualTo(new int[] {5,4,3,2,1}));
+    }
+  }
+
+  public static class Kata
+  {
+    public static int[] ReverseSeq(int n)
+    {
+      var returnArray = new int[n];
+
+      for(var i =  n; i > 0; i--)
+      {
+          returnArray[ n - i ] = i;
+      }
+
+      return returnArray;
+    }
+  }
+}
