64a6e9ed7e44e5cb63924c1ddebefcb827589473
Matt Goff <matt@goff.us>
2021-01-04 15:24:46 -0700

    mod4regex - sucks

---
+++ mod4regex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace mod4regex
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    //   var result = Mod.MOD4("[+05620]");
+    }
+  }
+
+  public class Mod
+  {
+    public static Regex MOD4 = new Regex(".*\\[[+-]?([048]|[0-9]*([02468][048]|[13579][26]))\\]");
+  }
+}
