ca9b7ded61d7238f2e303d6d65f4061c4816ce0e
Matt Goff <matt@goff.us>
2021-01-04 15:03:44 -0700

    higest rank number in an array

---
+++ hightest_rank_number_in_an_array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace hightest_rank_number_in_an_array
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      var arr = new int[] { 3, 5, 3, 6, 3, 3, 7, 4, 6, 3, 4, 5, 5 };
+      System.Console.WriteLine($"{HighestRank(arr)} should be 12");
+    }
+
+    public static int HighestRank(int[] arr)
+    {
+        var lq = from n in arr
+                    group n by n into grp
+                    orderby grp.Count() descending, grp.Key descending
+                    select new {key = grp.Key, cnt = grp.Count()};
+
+        var result = lq.FirstOrDefault();
+
+      return result.key;
+    }
+  }
+}
