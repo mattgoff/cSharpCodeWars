6968ba491d0fa2ddc2e3d840c05f1e4907378857
Matt Goff <matt@goff.us>
2021-01-10 10:55:46 -0700

    equalize_the_array

---
+++ equalize_the_array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace equalize_the_array
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      var results = Equalize(new[] { 10, 12, 24, 50, 0, 15, 20 });
+
+      System.Console.WriteLine($"Results : {String.Join(",", results)}");
+    }
+
+    public static string[] Equalize(int[] array)
+    {
+      if (array.Length == 0) return new string[0];
+
+      var returnArr = new String[array.Length];
+      returnArr[0] = "+0";
+
+      for (int i = 1; i < array.Length; i++)
+      {
+        // var returnSign = (array[i] - array[0] > 0 ? "+" : "-");
+        var data = (array[i] - array[0]).ToString("+#;-#;+0");
+        returnArr[i] = $"{data}";
+      }
+
+      return returnArr;
+    }
+
+    public static string[] Equalize2(int[] array)
+    {
+      return array.Select(x => (x - array[0]).ToString("+#;-#;+0")).ToArray();
+    }
+  }
+}
---
+++ number_of_proper_fractions_with_denominator_d/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace number_of_proper_fractions_with_denominator_d
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      System.Console.WriteLine($"Expected -> 0 Got -> {ProperFractions(1)}");
+      System.Console.WriteLine($"Expected -> 1 Got -> {ProperFractions(2)}");
+      System.Console.WriteLine($"Expected -> 4 Got -> {ProperFractions(5)}");
+      System.Console.WriteLine($"Expected -> 8 Got -> {ProperFractions(15)}");
+      System.Console.WriteLine($"Expected -> 20 Got -> {ProperFractions(25)}");
+
+    }
+
+    public static long ProperFractions(long n)
+    {
+        if (n==1)
+        {
+            return 0;
+        }
+
+        long result = n;
+        for (long i = 2; i * i <= n; i++)
+        {
+            if(n % i == 0)
+            {
+                while(n % i == 0)
+                {
+                    n /= i;
+                }
+                result -= result / i;
+            }
+        }
+        if(n > 1)
+        {
+            result -= result / n;
+        }
+
+        return result;
+    }
+  }
+}
