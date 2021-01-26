b5bf25d345b48cb1d0cb4cd647db8a989afd2560
Matt Goff <matt@goff.us>
2021-01-26 16:15:44 -0700

    grashopper_grade_book

---
+++ grasshopper-grade-book/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace grasshopper_grade_book
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      var results = GetGrade(95, 90 , 93);
+      System.Console.WriteLine($"Expected: A Actual: {results}");
+    }
+    public static char GetGrade(int s1, int s2, int s3)
+    {
+      var avg = (s1 + s2 + s3) / 3;
+        switch(avg)
+        {
+            case int n when (avg >= 90 && avg <= 100):
+                return 'A';
+            case int n when (avg >= 80 && avg < 90):
+                return 'B';
+            case int n when (avg >= 70 && avg < 80):
+                return 'C';
+            case int n when (avg >= 60 && avg < 70):
+                return 'D';
+        }
+      return 'F';
+    }
+  }
+}
